8a66cba87c61b5c0b89ea69997eae715a10995a5
Santiago Seira <sseira@stanford.edu>
2015-11-09 23:04:39 -0800

    done with clustering

--- songClustering.py
+++ songClustering.py
@@ -1,4 +1,6 @@
 import json
+import Queue 
+
 
 def getSongsForArtist(artist):
 	file_name = artist + '_lyrics.json'
@@ -20,8 +22,84 @@ def makeFeatureVectorForSong(song):
 			featureVector[word] = 1
 	return featureVector
 
+def songDistance(featureVector1, featureVector2):
+	dotProduct = 0.0
+	for feature1 in featureVector1:
+		for feature2 in featureVector2:
+			if feature1 == feature2:
+				dotProduct += featureVector1[feature1] * featureVector2[feature2]
+
+	return dotProduct
 
 
+def makeSongsFeatureVector(songs):
+	songsFeatureVectors = {}
+	for song in songs: 
+		featureVector = makeFeatureVectorForSong(songs[song])
+		songsFeatureVectors[song] = featureVector	
+	return songsFeatureVectors
+
+
+def makeSongRelevance(keywords, songs, songsFeatureVectors):
+	keywordFeatureVector = makeFeatureVectorForSong(keywords)
+
+	# return idealSongs which are the songs that best match with keywords
+	# idealSongs will be used as a reference to rate other songs 
+	def getIdealSongs(maxIdealSongs = 4):
+		idealSongs = Queue.PriorityQueue()
+		# minIdealDistance = None
+		for song in songs:
+			distance = songDistance(keywordFeatureVector, songsFeatureVectors[song])
+			if idealSongs.qsize() < maxIdealSongs:
+				idealSongs.put((distance, song))
+			else:
+				lowest = idealSongs.get()
+				if lowest[0] < distance:
+					idealSongs.put((distance, song))
+				else:
+					idealSongs.put(lowest)		
+
+		#normalize weights
+		minWeight = None
+		maxWeight = None
+		idealSongsDict = {}
+		# print idealSongsDict
+		while not idealSongs.empty():
+				weight, song = idealSongs.get()
+				if minWeight is None:
+					minWeight = weight
+				maxWeight = weight	
+				idealSongsDict[song] = weight
+
+		for song in idealSongsDict:
+			normalizedWeight = (idealSongsDict[song] - minWeight)/(maxWeight - minWeight)
+			idealSongsDict[song] = normalizedWeight + 2 # range [2:3]
+
+		return idealSongsDict
+
+
+	idealSongs = getIdealSongs()
+	songRelevances = {}
+	minRelevance = float('inf')
+	maxRelevance = float('-inf')
+	for title in songs:
+		relevance = 0
+		for idealTitle in idealSongs:
+			distance = songDistance(songsFeatureVectors[idealTitle], songsFeatureVectors[title])
+			relevance += distance*idealSongs[idealTitle]
+
+		songRelevances[title] = relevance
+		
+		if relevance > maxRelevance:
+			maxRelevance = relevance
+		if relevance < minRelevance:
+			minRelevance = relevance
+
+	for title in songRelevances:
+ 		normalizedWeight = (songRelevances[title] - minRelevance)/(maxRelevance - minRelevance)
+ 		songRelevances[title] = normalizedWeight + 1 # range [1:2]
+
+	return songRelevances
 
 
 
@@ -33,9 +111,7 @@ eminem = 'eminem'
 
 
 songs = getSongsForArtist(jayz)
-for song in songs: 
-	print songs[song]
-	featureVector = makeFeatureVectorForSong(songs[song])
-	print featureVector
-	print ""
-	print ""
+songsFeatureVectors = makeSongsFeatureVector(songs)
+makeSongRelevance('bitches cash', songs, songsFeatureVectors)
+
+	
